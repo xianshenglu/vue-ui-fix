7048f5902fe2bf35ad04737f001188042d80d455
luxiansheng <luxiansheng@zto.cn>
2019-05-12 20:31:36 +0800

    use gulp to build styles

---
+++ build/gulp.style.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const cleanCSS = require('gulp-clean-css')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const browsers = require('../package.json').browserslist
+exports.default = function () {
+  return gulp
+    .src('../packages/styles/*.scss')
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers
+      })
+    )
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('../lib/'))
+}

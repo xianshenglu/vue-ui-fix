da357b761b5a6c465a517924f55e54874c397eb2
luxiansheng <luxiansheng@zto.cn>
2019-05-18 16:14:22 +0800

    test passed with defaultExport

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { Button, Select } from 'element-ui'
+import { Select, Option, Checkbox } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(Button)
+import VueUiFix from 'vue-ui-fix'
+import 'vue-ui-fix/lib/vue-ui-fix.css'
 Vue.use(Select)
+Vue.use(Option)
+Vue.use(Checkbox)
 
+Vue.use(VueUiFix)
+// Vue.use(ElSelect)
+// Vue.use(HelloWorld)
 Vue.config.productionTip = false
 
 new Vue({

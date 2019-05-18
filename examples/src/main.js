2bc59cb91d43a472cd6eb081f2ce2797c6fe9a66
luxiansheng <luxiansheng@zto.cn>
2019-05-18 12:21:35 +0800

    add element-ui

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { Button, Select } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Button)
+Vue.use(Select)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')

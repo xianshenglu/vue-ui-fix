0aac80b567e444c6a13c4d073a8aca07976391ff
luxiansheng <luxiansheng@zto.cn>
2019-05-19 15:28:20 +0800

    remove unused config

--- build/config.js
+++ build/config.js
@@ -1,36 +1,11 @@
 let path = require('path')
 let nodeExternals = require('webpack-node-externals')
-let Components = require('../components.json')
 
-let externals = {}
-
-// todo correct?
-
-Object.keys(Components).forEach(function (key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`
-})
-
-externals = [
-  Object.assign(
-    {
-      vue: 'vue'
-    },
-    externals
-  ),
-  nodeExternals()
-]
-
-exports.externals = externals
+exports.externals = [nodeExternals()]
 
 exports.alias = {
   main: path.resolve(__dirname, '../src'),
   packages: path.resolve(__dirname, '../packages')
 }
 
-exports.vue = {
-  root: 'Vue',
-  commonjs: 'vue',
-  commonjs2: 'vue',
-  amd: 'vue'
-}
 exports.jsexclude = /node_modules/
